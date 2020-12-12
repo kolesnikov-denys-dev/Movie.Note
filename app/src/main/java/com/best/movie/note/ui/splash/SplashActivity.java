7b975b89a528cda136ba0f108a047d0fda7d54bd
Denys <kolesnikov.denys.dev@gmail.com>
2020-12-12 02:10:34 +0200

    Implemented:  - added SplashActivity

--- app/src/main/java/com/best/movie/note/ui/splash/SplashActivity.java
+++ app/src/main/java/com/best/movie/note/ui/splash/SplashActivity.java
@@ -1,22 +1,41 @@
 package com.best.movie.note.ui.splash;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.best.movie.note.MainActivity;
 import com.best.movie.note.R;
 
-public class SplashActivity extends Activity {
-
-    private TextView mTextView;
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
 
-        // Enables Always-on
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+            }
+        }, 2000);
     }
+
 }
